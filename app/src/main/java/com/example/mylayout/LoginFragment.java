ed9dbc8e262a241f3988c11a0e66789145bd02af
shadowAnt <1203355251@qq.com>
2017-03-22 15:55:23 +0800

    完成登录界面及跳转

--- app/src/main/java/com/example/mylayout/LoginFragment.java
+++ app/src/main/java/com/example/mylayout/LoginFragment.java
@@ -1,12 +1,15 @@
 package com.example.mylayout;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+
+import space.wangjiang.toaster.Toaster;
 
 /**
  * Created by ShadowAnt on 2017/3/21.
@@ -32,6 +35,15 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.login:
                 progressBar.setVisibility(View.VISIBLE);
+                Toaster.success(getActivity(), "登录成功 !", Toaster.LENGTH_SHORT).show();
+
+                //点击按钮生成一个Intent，传递信息给HomeActivity
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                intent.putExtra("send", "登录成功!");
+                startActivity(intent);
+
+                //结束掉登录界面活动
+                getActivity().finish();
                 break;
             default:
         }
